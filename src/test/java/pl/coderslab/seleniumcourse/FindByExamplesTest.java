9589b45eb3b54f2375f22e29edfdc2641b91bc3c
edu <edu@mail.pl>
2022-06-26 15:29:23 +0200

    find by id

---
+++ src/test/java/pl/coderslab/seleniumcourse/FindByExamplesTest.java
@@ -0,0 +1,25 @@
+package pl.coderslab.seleniumcourse;
+
+import org.junit.jupiter.api.Test;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+import static pl.coderslab.seleniumcourse.Tools.fillInput;
+
+public class FindByExamplesTest {
+    @Test
+    public void shouldFindElementsById() {
+        System.setProperty("webdriver.chrome.driver", "src/test/resources/drivers/chromedriver.exe");
+        WebDriver driver = new ChromeDriver();
+        driver.get("https://hotel-testlab.coderslab.pl/en/");
+        WebElement hotelLocationInput = driver.findElement(By.id("hotel_location"));
+        fillInput(hotelLocationInput, "Warsaw");
+        WebElement searchNowButton = driver.findElement(By.id("search_room_submit"));
+        WebElement footerEmailInput = driver.findElement(By.id("newsletter-input"));
+        fillInput(footerEmailInput, "someone@mail.pl");
+
+        //driver.quit();
+    }
+}
--- src/test/java/pl/coderslab/seleniumcourse/Tools.java
+++ src/test/java/pl/coderslab/seleniumcourse/Tools.java
@@ -2,6 +2,8 @@ package pl.coderslab.seleniumcourse;
 
 // usage: Tools.sleep(2000);
 
+import org.openqa.selenium.WebElement;
+
 public class Tools {
     public static void sleep(long sleepTimeInMs) {
         try {
@@ -9,5 +11,10 @@ public class Tools {
         } catch (InterruptedException e) {
             throw new RuntimeException(e);
         }
+    }
+
+    public static void fillInput(WebElement input, String text) {
+        input.clear();
+        input.sendKeys(text);
     }
 }
