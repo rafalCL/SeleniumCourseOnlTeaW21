4d03b5161d773b92aa38929888fa0772b0332c9f
edu <edu@mail.pl>
2022-07-09 10:57:17 +0200

    example multiple tests using the same page object, tests are better readable now

--- src/test/java/pl/coderslab/seleniumcourse/pageobject/ddg/DuckDuckGoSearchPageObjectedTest.java
+++ src/test/java/pl/coderslab/seleniumcourse/pageobject/ddg/DuckDuckGoSearchPageObjectedTest.java
@@ -1,19 +1,37 @@
 package pl.coderslab.seleniumcourse.pageobject.ddg;
 
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
-import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
-import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
 
 public class DuckDuckGoSearchPageObjectedTest {
-    @Test
-    public void shouldSearchWithDDG() {
+    private WebDriver driver;
+    private MainDdgPage mainDdgPage;
+
+    @BeforeEach
+    public void beforeEach() {
         System.setProperty("webdriver.chrome.driver", "src/test/resources/drivers/chromedriver.exe");
-        WebDriver driver = new ChromeDriver();
+        this.driver = new ChromeDriver();
+        this.mainDdgPage = new MainDdgPage(driver);
+    }
+
+    @Test
+    public void shouldSearchWPustyniWithDDG() {
+        // given
         driver.get("https://duckduckgo.com/");
-        MainDdgPage mainDdgPage = new MainDdgPage(driver);
+        // when
         mainDdgPage.searchPhrase("W pustyni i w puszczy");
+        // then
+        // assert
+        //driver.quit();
+    }
+
+    @Test
+    public void shouldSearchBobkowskiWithDDG() {
+        driver.get("https://duckduckgo.com/");
+        mainDdgPage.searchPhrase("Szkice piórkiem Andrzej Bobkowski");
+        // assert
         //driver.quit();
     }
 }
