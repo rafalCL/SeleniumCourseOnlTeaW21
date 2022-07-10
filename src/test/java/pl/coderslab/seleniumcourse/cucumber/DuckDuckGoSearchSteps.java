cb42ecaa5914eda140066d3dfe597f7980409c2c
edu <edu@mail.pl>
2022-07-10 12:37:40 +0200

    fix indentation

--- src/test/java/pl/coderslab/seleniumcourse/cucumber/DuckDuckGoSearchSteps.java
+++ src/test/java/pl/coderslab/seleniumcourse/cucumber/DuckDuckGoSearchSteps.java
@@ -23,26 +23,26 @@ public class DuckDuckGoSearchSteps {
         this.driver.get("https://duckduckgo.com/");
     }
 
-  @When("Entered \"Henryk Sienkiewicz\" on the search input box")
-  public void fillSearchInputBox() {
-      WebElement searchInput = driver.findElement(By.name("q"));
-      searchInput.sendKeys("Henryk Sienkiewicz");
-  }
+    @When("Entered \"Henryk Sienkiewicz\" on the search input box")
+    public void fillSearchInputBox() {
+        WebElement searchInput = driver.findElement(By.name("q"));
+        searchInput.sendKeys("Henryk Sienkiewicz");
+    }
 
-  @And("Search button \\(magnifying glass\\) clicked")
+    @And("Search button \\(magnifying glass\\) clicked")
     public void clickSearchButton() {
-      WebElement searchButton = driver.findElement(By.id("search_button_homepage"));
-      searchButton.click();
-  }
+        WebElement searchButton = driver.findElement(By.id("search_button_homepage"));
+        searchButton.click();
+    }
 
     @Then("First 3 search results contain phrase: \"Henryk Sienkiewicz\"")
     public void firstResultsContainPhrase() {
         List<WebElement> results = driver.findElements(By.cssSelector(".nrn-react-div"));
         String expectedPhrase = "Henryk Sienkiewicz";
-        for(int i=0; i<3; i++) {
+        for (int i = 0; i < 3; i++) {
             WebElement currentResultElement = results.get(i);
             WebElement currentResultHeader = currentResultElement.findElement(By.cssSelector("h2"));
-            if(!currentResultHeader.getText().contains(expectedPhrase)) {
+            if (!currentResultHeader.getText().contains(expectedPhrase)) {
                 fail(String.format("Phrase %s NOT found in search result: %s",
                         expectedPhrase, currentResultHeader.getText()));
             }
